340eacc00b87e3c09d7db278b42310dd566715e2
hannah26hannah <ooyoonoo24@gmail.com>
2020-10-11 18:34:20 +0900

    Create status bar component using vue.js

---
+++ status-bar.js
@@ -0,0 +1,41 @@
+
+
+Vue.component('status-bar', {
+    template: `
+    <div class="status-bar">
+      <div class="status-bar__column">
+        <span class="status-bar__colck">{{ timestamp }}</span>
+      </div>
+      <div class="status-bar__column">
+        <i class="fas fa-clock"></i>
+        <i class="fas fa-volume-mute"></i>
+        <i class="fas fa-wifi"></i>
+        <span class="status-bar__battery-level">100%</span>
+        <i class="fas fa-battery-full"></i>
+      </div>
+    </div>
+    `,
+    data: function() {
+        timestamp = ''
+        return {
+            timestamp
+        }
+    },
+    created() {
+        setInterval(this.getNow, 1000);
+    },
+    methods: {
+        getNow: function() {
+            let d = new Date();
+            let hour = d.getHours();
+            let minute = d.getMinutes(); 
+            let second = d.getSeconds();
+            let date = `${hour < 10 ? "0" + hour : hour}`+":"+`${minute < 10 ? "0" + minute : minute}`;
+            this.timestamp = date;
+        }
+    }
+  })
+  var app = new Vue({
+    el: '#headerBar'
+  })
+
