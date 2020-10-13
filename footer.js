6bc6e665b044efe009d800b73e840fa9a01601f4
hannah26hannah <ooyoonoo24@gmail.com>
2020-10-13 12:48:50 +0900

    Created footer

---
+++ footer.js
@@ -0,0 +1,30 @@
+Vue.component('footer-bar', {
+    template: `
+    <ul class="nav__list">
+      <li class="nav__list-item">
+        <a href="friends.html" class="nav__list-link">
+          <i class="far fa-user"></i>
+        </a>
+      </li>
+      <li class="nav__list-item">
+        <a href="index.html" class="nav__list-link">
+          <div class="nav__badge">8</div>
+          <i class="fas fa-comment"></i>
+        </a>
+      </li>
+      <li class="nav__list-item">
+        <a href="find.html" class="nav__list-link">
+          <i class="fas fa-search"></i>
+        </a>
+      </li>
+      <li class="nav__list-item">
+        <a href="more.html" class="nav__list-link">
+          <i class="fas fa-ellipsis-h"></i
+        ></a>
+      </li>
+    </ul>
+    `
+})
+var app = new Vue({
+    el: '#footerBar'
+})
